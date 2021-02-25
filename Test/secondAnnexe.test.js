b179d964238e2f13bf4eefc10090a0302c1e4645
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-02-25 20:46:35 +0100

    Adding the second annexe tester I hope this is what should be done

--- Test/secondAnnexe.test.js
+++ Test/secondAnnexe.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 const secondAnnexe = require('../Annexes/annexe2');
 
-describe('Second annexe tests', () => {
+describe('Testing the Second annexe', () => {
 	it('Should returns 15', () => {
 		const date_start = '2020-06-10';
 		const date_end = '2020-07-25';
@@ -15,14 +15,26 @@ describe('Second annexe tests', () => {
 		const date_start = '2020-03-15';
 		const date_end = '2020-04-14';
 
-		console.log(secondAnnexe.SecondAnnexeSolution(date_start, date_end));
 		assert.strictEqual(secondAnnexe.SecondAnnexeSolution(date_start, date_end), 30);
 	})
 
-	it('Should returns ', () => {
-		const date_start = '2020-03-15';
-		const date_end = '2020-04-14';
+	it('Should returns 41', () => {
+		const date_start = '2020-01-20';
+		const date_end = '2020-08-9';
 
-		assert.strictEqual(secondAnnexe.SecondAnnexeSolution(date_start, date_end), 30);
+		assert.strictEqual(secondAnnexe.SecondAnnexeSolution(date_start, date_end), 41);
+	})
+
+	it('Should Throw an error Missing parameter', () => {
+		const date_end = '2020-08-9';
+
+		assert.notStrictEqual(secondAnnexe.SecondAnnexeSolution(date_end), 41);
+	})
+
+	it('Should Throw an error Not a date paramter', () => {
+		const date_end = '2020-08-9';
+		const date_start = '20200202020';
+
+		assert.notStrictEqual(secondAnnexe.SecondAnnexeSolution(date_start, date_end), "");
 	})
 })
